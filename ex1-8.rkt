33a0f2383ef2573ae6ef7a0501e0db066d31b9f6
w <wylliam.bastiani@gmail.com>
2020-10-16 17:25:07 -0300

    exercise: 1.8

--- ex1-8.rkt
+++ ex1-8.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
-;; Newton Cube-Root algorithm
+;; Exercise 1.8: Newton’s method for cube roots is based on the 
+;; fact that if y is an approximation to the cube root of x, 
+;; then a better approximation is given by the value x/y2+2y3.
+;; Use this formula to implement a cube-root procedure analogous to the square-root procedure.
 
 (define (cube-root n)
 
